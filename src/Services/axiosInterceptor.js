ca45c686b8c3608378379fc51cda8203baa1efbe
muqaddasawan <awan.gee888@gmail.com>
2023-06-27 19:49:35 +0500

    update

--- src/Services/axiosInterceptor.js
+++ src/Services/axiosInterceptor.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.thebaklavaboxx.co.uk/",
+  headers: {
+    "Access-Control-Allow-Origin": "https://api.thebaklavaboxx.co.uk/",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    // "Content-Type": "application/json",
+  },
+  withCredentials: true,
   timeout: 5000,
 });
 
-//on localhost change server backend http://localhost:8000/
+//on localhost change server backend http:://localhost:8000/
+//https:://api.thebaklavaboxx.co.uk/
+//https://thebaklavaboxx.co.uk
 
 export default instance;
